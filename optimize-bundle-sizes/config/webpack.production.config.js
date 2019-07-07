6fd87a7c6cc33a218b490323fa2be14129aa0244
Victor <azeta351@gmail.com>
2019-07-07 22:27:09 +0200

    feat(optimize-bundle-sizes): add cdn externals

--- optimize-bundle-sizes/config/webpack.production.config.js
+++ optimize-bundle-sizes/config/webpack.production.config.js
@@ -1,6 +1,28 @@
 const merge = require('webpack-merge')
+const WebpackCdnPlugin = require('webpack-cdn-plugin');
+
 const base = require('./webpack.base.config');
 
 module.exports = merge(base, {
-    mode: 'production'
+    mode: 'production',
+    externals: {
+        react: 'React',
+        'react-dom': 'ReactDOM'
+    },
+    plugins: [
+        new WebpackCdnPlugin({
+            modules: [
+                {
+                    name: 'react',
+                    var: 'React',
+                    path: 'umd/react.production.min.js'
+                },
+                {
+                    name: 'react-dom',
+                    var: 'ReactDOM',
+                    path: 'umd/react-dom.production.min.js'
+                },
+            ]
+        }),
+    ]
 });
