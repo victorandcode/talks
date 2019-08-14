f6bf08b471c93d8d4d1163784a8428bfe18f7021
Victor <azeta351@gmail.com>
2019-08-14 08:09:23 +0200

    feat(frontend-fundamentals): add css exercise

--- frontend-fundamentals/assert.js
+++
@@ -1,4 +0,0 @@
-module.exports.assert = (actual, expected) =>
-    actual === expected
-        ? console.log('All good')
-        : console.error(`Test failed: expected ${expected} but got ${actual}`);
