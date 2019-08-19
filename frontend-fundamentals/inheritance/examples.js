c1bedeba7843a069a69190466012f3f6a903d8e6
Victor <azeta351@gmail.com>
2019-08-19 08:15:39 +0200

    feat(frontend-fundamentals): clean comments from inheritance example

--- frontend-fundamentals/inheritance/examples.js
+++ frontend-fundamentals/inheritance/examples.js
@@ -1,23 +1,4 @@
 /**
- * Questions to answer:
- * - Object.create do
- * - What is the value of instance.prototype
- * - How does an attribute get resolved
- * - When to assign to obj.prototype.myMethod
- *  How can I call attributes from the parent
- */
-
-/**
- * 
- * Practicar y dominar los conceptos detrás de inheritance con prototypes
-  Usando el operador de instanceof, isPrototypeOf
-  getPrototypeof
-  https://stackoverflow.com/questions/38740610/object-getprototypeof-vs-prototype
-  https://github.com/getify/You-Dont-Know-JS/blob/master/this%20%26%20object%20prototypes/ch5.md
-  https://github.com/getify/You-Dont-Know-JS/blob/fe3ea179f1efd2c649789f713ead9ed765a4d80b/this%20%26%20object%20prototypes/ch6.md
- */
-
-/**
  * Parasitic inheritance
  */
 function Animal(name) {
